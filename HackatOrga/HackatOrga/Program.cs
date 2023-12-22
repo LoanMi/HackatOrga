50ca81fe3044bf960d6cc1bf46769b709ce975f8
AnArcticPigeon <an.arctic.pigeon@gmail.com>
2023-12-22 13:14:07 +0100

    Adrien pas d'erreurs

--- HackatOrga/HackatOrga/Program.cs
+++ HackatOrga/HackatOrga/Program.cs
@@ -11,7 +11,7 @@ namespace HackatOrga
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Menu());
+            Application.Run(new Create());
         }
     }
 }
